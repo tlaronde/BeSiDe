2ec17bb54e11ffea4a9e72d075a2de18dc422102
Thierry LARONDE <tlaronde@kergis.com>
2024-10-03 12:31:29 +0200

    Path searching: qualified filenames with not ending dot restriction.
    I had initially, because it made for a simple test, restricted
    qualified filenames (for the optional extended path searching for
    bar/foo like identifiers) to forbid any trailing dot in any component
    of the identifier.
    
    I have removed the specification so now any non absolute nor relative
    identifier is allowed, including with components ending with a dot.
    The test is more complex, but is written so that it hopefully gives
    rapidly the answer at least with simple identifiers (no '/' or no '.'
    in the identifier).

--- include/pathsearch.h
+++ include/pathsearch.h
@@ -22,13 +22,43 @@
 /*
  * A qualified filename should be considered a Resource Identifier
  * to be contrasted to a Locator (where a resource lies in the
- * namespace). See pathsearch(7).
+ * namespace). So it has to be non rooted and without walking path
+ * directives ("." or ".."). See pathsearch(7).
+ * Forbidding any component of the identifer to end with a dot will
+ * make the test more easier. But, for now, don't restrict
+ * identifiers.
+ * We try in this to succeed or fail early for traditional last
+ * leaf identifiers.
  */
 #define PATH_SEARCH_IS_QFILENAME(name, name_len) \
-	( (name) != NULL && (name_len) != 0 \
-		&& (name)[0] != '/' && strstr(name, "./") == NULL \
-		&& (name)[(name_len)-1] != '/' \
-		&& (name)[(name_len)-1] != '.' )
+( (name) != NULL && (name_len) != 0 \
+  && (name)[0] != '/' \
+  && (name)[name_len-1] != '/' \
+  && ( \
+    (name_len == 1 && (name)[0] != '.') \
+    || ( name_len == 2 && ( (name)[0] != '.' || (name)[1] != '.' ) ) \
+    || ( name_len >= 3 \
+         && ( \
+           ( strstr(name, "/") == NULL || strstr(name, ".") == NULL ) \
+           || ( \
+              strncmp(name, "./", 2) != 0 \
+              && strncmp(name, "../", 3) != 0 \
+              && strstr(name, "/./") == NULL \
+              && strstr(name, "/../") == NULL \
+              && ( \
+                 (name)[name_len-1] != '.' \
+                   || (  (name)[name_len-2] != '/' \
+                      && ( \
+                           (name)[name_len-2] != '.' \
+                           || (name)[name_len-3] != '/' \
+                         ) \
+                      ) \
+                 ) \
+	      ) \
+           ) \
+       ) \
+     ) \
+)
 
 #endif /* !_PATHSEARCH_H_ */
 
